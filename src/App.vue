f5becec5a10b4cdd1e87ff791d5ada075b82ad9c
Nguyen Thanh Luan <luann4099@gmail.com>
2022-03-31 16:31:09 +0700

    chua bt

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="gallery">
+    <div v-for="(item,index) in images" :key="index">
+      <img @click="selectedImage=item" :src="item"/>
+    </div>
+    <div class="modal" :class="{'hidden':!selectedImage}">
+      <div class="inner-modal">
+        <img :src="selectedImage"/>
+      </div>
+      <button @click="_closeModal()" class="close-btn">Close</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      selectedImage: null,
+      images: [
+        require('./assets/img/1.jpeg'),
+        require('./assets/img/2.jpg'),
+        'https://images.all-free-download.com/images/graphiclarge/beautiful_natural_scenic_03_hd_picture_166230.jpg'
+      ]
+    }
+  },
+  methods: {
+    _closeModal() {
+      this.selectedImage = null;
+    },
+    _addEscapeEvent() {
+      return (e) => {
+        if (e.key === 'Escape') {
+          this._closeModal()
+        }
+      };
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      document.addEventListener('keyup',this._addEscapeEvent());
+    }, 1000);
+  },
+  onDestroy() {
+    document.removeEventListener('keyup', this._addEscapeEvent());
+  },
+  components: {}
 }
 </script>
 
@@ -24,5 +58,37 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.gallery {
+  display: grid;
+  grid-template-columns: auto auto auto;
+}
+
+.gallery img {
+  width: 10rem
+}
+
+.modal {
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.47);
+}
+
+.hidden {
+  display: none;
+}
+
+.inner-modal {
+  position: relative;
+}
+
+.close-btn {
+  position: absolute;
+  top: 0;
+  right: 0;
 }
 </style>
